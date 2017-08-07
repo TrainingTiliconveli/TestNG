4726c9628b3b267d71a25ddd8368ce34bf26f6e7
Suriya <training@tiliconveli.com>
2017-08-07 05:10:09 -0400

    My First Commit

---
+++ example/src/example/Jen.java
@@ -0,0 +1,13 @@
+package example;
+
+import org.testng.annotations.Test;
+
+public class Jen {
+@Test
+public void a()
+{
+	System.out.println("hello jenkin");
+	System.out.println("hello jenkin");
+	System.out.println("haiiiii");
+}
+}
